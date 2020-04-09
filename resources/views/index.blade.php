f1f8ca43c541404fe5724c3c4388aac11e32bc71
Max Orlov <maksimorlov.service@gmail.com>
2020-04-09 19:49:02 +0300

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,7 +84,7 @@
 
 <pre>
 if (! function_exists('sms_code')) {
-    function sms_code(string $message) :? string {
+    function sms_code(string $message) :? int {
         $message = " {$message} ";
         $match = preg_match('/\b[\d]{4}[\s\n]/', $message, $matches);
         return isset($matches[0]) ? trim($matches[0]) : null;
